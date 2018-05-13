6f33136bb0d3e50569be86ed8431a3991833e33f
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-05-12 20:52:21 -0400

    adds cat command

--- web/src/components/TerminalBody.js
+++ web/src/components/TerminalBody.js
@@ -34,24 +34,83 @@ class TerminalBody extends Component {
   }
 
   handleUserCommand = command => {
-    switch (command) {
-      case "help":
+    switch(command) {
+      case 'help':
         this.appendTerminalContent(`
-          <br/><br/>
-          GNU bash, version 3.2.57(1)-release (x86_64-apple-darwin17)<br/>
-          These shell commands are defined internally.  Type 'help' to see this list.<br/><br/>
+          <br><br>
+          GNU bash, version 3.2.57(1)-release (x86_64-apple-darwin17)<br>
+          These shell commands are defined internally.  Type 'help' to see this list.<br><br>
           <div style="width: 400px; display: inline-block;">
             <div style="width: 200px; display: inline-block; float: left;">
-              COMMAND<br/>
+              COMMAND<br>
               ls
             </div>
             <div style="width: 200px; display: inline-block; float: left;">
-              DESCRIPTION<br/>
+              DESCRIPTION<br>
               List directory contents
             </div>
           </div>
         `)
-        break;
+      case 'ls':
+        this.appendTerminalContent(`
+          <br><br>
+          <strong style="margin-right: 10px;">Projects</strong>
+          <strong style="margin-right: 10px;">Portfolio</strong>
+          <strong style="margin-right: 10px;">Blog</strong>
+          <p style="display: inline-block;">about.txt<p>
+        `)
+        break
+      case 'cat about.txt':
+        this.appendTerminalContent(`
+          <br><br>
+          I'm a software engineer based in New York City with a passion for technology.
+          I work mainly in the web development industry, but I love spending my free time
+          exploring other forms/platforms for programming, whether it be embedded systems
+          or game development. Here's some of what I've got under my belt: <br>
+          <div style="width: 600px; display: inline-block">
+            <div style="width: 150px; float: left">
+              <p><strong>Languages</strong></p>
+              <ul>
+                <li>Ruby</li>
+                <li>Javascript</li>
+                <li>Python</li>
+                <li>Elixir</li>
+                <li>PHP</li>
+                <li>HTML & CSS</li>
+              </ul>
+            </div>
+            <div style="width: 150px; float: left">
+              <p><strong>Frameworks</strong></p>
+              <ul>
+                <li>Ruby on Rails</li>
+                <li>Sinatra</li>
+                <li>React</li>
+                <li>AngularJS</li>
+                <li>Sass</li>
+                <li>Phoenix</li>
+              </ul>
+            </div>
+            <div style="width: 150px; float: left">
+              <p><strong>Database</strong></p>
+              <ul>
+                <li>MySQL</li>
+                <li>PostgresQL</li>
+                <li>MongoDB</li>
+                <li>LevelDB</li>
+              </ul>
+            </div>
+            <div style="width: 150px; float: left">
+              <p><strong>Other</strong></p>
+              <ul>
+                <li>Wordpress</li>
+                <li>Amazon Web Services</li>
+                <li>Heroku</li>
+                <li>Git</li>
+              </ul>
+            </div>
+          </div>
+        `)
+        break
       default:
         this.appendTerminalContent(`<br><br>${command}: command not found. Try typing 'help' to get a list of commands.`)
     }
